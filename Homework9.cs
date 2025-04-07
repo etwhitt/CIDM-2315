f1ed1563c858cef1cf847b4f79023e88b65bc7fc
etwhitt <etwhitt1@buffs.wtamu.edu>
2025-04-07 08:26:22 -0500

    Update Homework9.cs

--- Homework9.cs
+++ Homework9.cs
@@ -4,11 +4,13 @@ public class Program
 {
     static void Main(string[] args)
     {
+        //Creation of four students
         new Student(111, "Alice");
         new Student(222, "Bob");
         new Student(333, "Cathy");
         new Student(444, "David");
 
+        //Creation of dictionary
         Dictionary<string, double> gradebook = new Dictionary<string, double>
         {
             { "Alice", 4.0 },
@@ -17,15 +19,19 @@ public class Program
             { "David", 1.8 }
         };
 
+        //Check if Tom is in gradebook and if not, inserting him with 3.3 GPA
         if (!gradebook.ContainsKey("Tom"))
         {
             gradebook["Tom"] = 3.3;
         }
 
+        //Calculation of average GPA of all students
         double averageGPA = gradebook.Values.Average();
 
+        //Print average GPA of all students
         Console.WriteLine($"The average GPA is: {averageGPA:F2}");
 
+        //Print student information for those with GPA greater than average
         foreach (Student student in Student.studentList)
         {
             string name = student.GetName();
